d5342aea16e9462cec9eca1eaf21e62401ecc62e
tonmoy <tonmoy3673@gmail.com>
2025-05-20 22:46:05 +0600

    :alien: load category data

--- src/Components/Shared/Navbar.tsx
+++ src/Components/Shared/Navbar.tsx
@@ -1,9 +1,11 @@
+import Sidebar from "./Sidebar";
 
 
 const Navbar = () => {
     return (
         <div>
             <h2> Navbar</h2>
+            <Sidebar/>
         </div>
     );
 };
--- src/Components/Shared/Sidebar.tsx
+++ src/Components/Shared/Sidebar.tsx
@@ -1,16 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const Sidebar = () => {
     const [categories,setCategories] = useState<string[]>([]);
-    const [keywords] = useState([
+    const [keywords] = useState<string[]>([
         "apple",
         "watch",
         "fashion",
         "trend",
         "shoes",
         "shirt"
-    ])
+    ]);
+
+    const [loading,setLoading] = useState<boolean>(false);
+    const [error,setError] = useState<string| null>(null)
+
+
+    // ============= Load Categories ========//
+    useEffect(()=>{
+        setLoading(true)
+        const loadCategories =async()=>{
+            const res = await fetch('https://dummyjson.com/products')
+            try{
+                if (!res.ok) {
+                    throw new Error('API response not found')
+                   
+                }
+                else{
+                const data = await res.json();
+                setCategories(data);
+                setError(null);
+                }
+
+            }
+            catch(err : unknown){
+                if(err instanceof Error){
+                   setError(err.message)
+                }
+            }
+            finally{
+                setLoading(false)
+            }
+        }
+        loadCategories();
+    },[])
+console.log(categories);
     return (
         <div>
             <h2>Sidebar</h2>
